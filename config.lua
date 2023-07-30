bfac761054ff893b4d877b197f64a734344a6e08
JordanBScott <99845881+JordanBScott@users.noreply.github.com>
2023-07-29 21:09:17 -0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -9,14 +9,14 @@ Config = {}
 
 Config.Framework = "QBCORE"             -- QBCORE o ESX
 Config.Debug     = false                 -- Debug mode
-Config.Logo =                           "https://i.imgur.com/yduIdh7.png" 
+Config.Logo =                           "https://cdn.discordapp.com/attachments/967905287673618512/1135060840391909446/NoBackgroundOrigional.png" 
 
 Config.OpenScore =                      "scoreboard"
 Config.ChatCommandSuggest =             "Open Scoreboard"
 Config.KeyOpen =                        "F10"
 Config.KeyName =                        "Open Scorebaord"
 
-Config.MaxPlayers =                     GetConvarInt('sv_maxclients', 70)
+Config.MaxPlayers =                     GetConvarInt('sv_maxclients', 64)
 Config.TypeIconMoney =                  " $"
 
 Config.NoJob =                          "unemployed"
@@ -29,12 +29,8 @@ Config.UseRobberys = true
 
 Config.RobList = {
     {order = 1 , label = 'FLEECA',          requieredCops = 3},
-    {order = 2 , label = 'PACIFIC BANK',    requieredCops = 2},
-    {order = 3 , label = 'VANGELICO',       requieredCops = 6},
-    {order = 4 , label = 'STORE',           requieredCops = 3},
-    {order = 5 , label = 'HUMANE LABS',     requieredCops = 4},
-    {order = 6 , label = 'BOBCAT',          requieredCops = 5},
-    {order = 7 , label = 'UNION',           requieredCops = 3},
-    {order = 8 , label = 'YACHT',           requieredCops = 2},
-    {order = 9 , label = 'VANT',            requieredCops = 1},
+    {order = 2 , label = 'PACIFIC BANK',    requieredCops = 5},
+    {order = 2 , label = 'PALETO BANK',     requieredCops = 4},
+    {order = 3 , label = 'JEWELRY STORE',   requieredCops = 3},
+    {order = 4 , label = 'STORE',           requieredCops = 2},
 }
